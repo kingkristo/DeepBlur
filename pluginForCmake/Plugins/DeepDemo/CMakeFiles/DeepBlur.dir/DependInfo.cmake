762c2542a004f4a401ef3f5bbbbdf76d6602be10
kingkristo <38288240+kingkristo@users.noreply.github.com>
2021-03-17 14:50:22 +0000

    Add files via upload

---
+++ pluginForCmake/Plugins/DeepDemo/CMakeFiles/DeepBlur.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/FiercePC/Documents/pluginForCmake/Plugins/DeepDemo/DeepBlur.cpp" "C:/Users/FiercePC/Documents/pluginForCmake/Plugins/DeepDemo/CMakeFiles/DeepBlur.dir/DeepBlur.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DeepBlur_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/Program Files/Nuke12.2v5/include"
+  "Plugins/DeepDemo"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ pluginForCmake/Plugins/DeepDemo/CMakeFiles/DeepBlur.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DeepBlur.dir/DeepBlur.cpp.obj"
+  "DeepBlur.dll"
+  "DeepBlur.dll.manifest"
+  "DeepBlur.lib"
+  "DeepBlur.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DeepBlur.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
